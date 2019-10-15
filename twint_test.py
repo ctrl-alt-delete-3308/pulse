a491596c5563244a50c14d7e8d7d7bb987bdc599
Rhett <rhett.hanscom@colorado.edu>
2019-10-15 12:04:40 -0600

    added some test stuff

---
+++ twint_test.py
@@ -0,0 +1,20 @@
+#to install, you will need pip3:
+# pip3 install twint
+# to output search results to textfile:
+# python3 twint_test.py > testText.txt
+
+import twint
+import numpy as np
+
+# Configure
+c = twint.Config()
+# c.Username = "noneprivacy"
+c.Search = "boulder"
+c.Format = "Username: {username} |  Tweet: {tweet}"
+c.Limit = 1
+c.Pandas = True
+
+twint.run.Search(c)
+# np.savetxt('data.csv', data, delimiter=',')
+# to output search results to textfile:
+# python3 twint_test.py > SomeFile.txt
