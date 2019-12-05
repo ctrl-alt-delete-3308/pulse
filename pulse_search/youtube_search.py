56f63b02bd045e410020574e7d8087bebf1215be
Ryan Drew <ryan@thedrews.org>
2019-12-04 18:49:41 -0700

    Load Youtube API key from env var

--- pulse_search/youtube_search.py
+++ pulse_search/youtube_search.py
@@ -9,7 +9,11 @@ def main(user_input):
 
     api_name = "youtube"                                 # use youtube api
     api_ver  = "v3"                                      # version 3
-    API_KEY  = "AIzaSyAjrL-bTZAQ9vQjsx_aCz2Xf93puSRN6Xo" # csci3308-pulse key
+    API_KEY  = os.getenv("PULSE_YOUTUBE_API_KEY", False) # csci3308-pulse key
+    if not API_KEY:
+        raise ValueError(
+            "Unable to get YouTube API key- is PULSE_YOUTUBE_API_KEY set?"
+        )
 
     # create object to use search and list below and store as "youtube"
     youtube = googleapiclient.discovery.build(api_name, api_ver, developerKey=API_KEY)
