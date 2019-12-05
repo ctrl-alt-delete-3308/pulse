98f6e5fe5b5585bfe6557ed56d11ef969f580f4f
Ryan Drew <ryan@thedrews.org>
2019-12-04 17:36:27 -0700

    Move 'whitenoise' middleware to second entry
    In `prod.py`, whitenoise was being appended to MIDDLEWARE when it should
    actually come just before `SecurityMiddleware`.

--- pulse_project/settings/prod.py
+++ pulse_project/settings/prod.py
@@ -41,7 +41,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Security Settings
 CSRF_COOKIE_SECURE = True
