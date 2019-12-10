ec07d35aa8e6ecc06dc07ab1c53955cff1543756
Rhett <rhett.hanscom@colorado.edu>
2019-12-10 14:00:06 -0700

    adding test cases to be run automatically when site is built

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from users.views import register, profile
+
 
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+
+    def test_register_url(self):
+        url = reverse('register')
+        self.assertEquals(resolve(url).func, register)
+
+    def test_profile_url(self):
+        url = reverse('profile')
+        self.assertEquals(resolve(url).func, profile)
